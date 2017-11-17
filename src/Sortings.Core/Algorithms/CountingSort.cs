4f7676eff8687dd864c200efc1e721d41d294ff2
Nick Strohm <nick.strohm99@gmail.com>
2017-11-17 23:01:39 +0100

    Cleanup and fix

--- src/Sortings.Core/Algorithms/CountingSort.cs
+++ src/Sortings.Core/Algorithms/CountingSort.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sortings.Core.Algorithms
@@ -8,30 +9,28 @@ namespace Sortings.Core.Algorithms
         internal override void Sort(int[] x, IDictionary<string, dynamic> parameters)
         {
             var n = x.Length;
-            var min = x.Min();
             var max = x.Max();
 
             var output = new int[n];
 
-            var count = new int[max - min + 1];
+            var count = new int[max + 1];
             for (var i = 0; i < x.Length; i++)
             {
-                count[x[i] - min]++;
+                count[x[i]]++;
             }
 
-            count[0]--;
-            for (var i = 1; i < count.Length; i++)
+            var currIndex = 0;
+
+            for (var i = 0; i < max; i++)
             {
-                count[i] += count[i - 1];
+                for (var j = 0; j < count[i]; j++)
+                {
+                    output[currIndex] = i;
+                    currIndex++;
+                }
             }
 
-            for (var i = 0; i < n; i++)
-            {
-                output[count[x[i] - min]--] = x[i];
-            }
-
-            // ReSharper disable once RedundantAssignment
-            x = output;
+            Array.Copy(output, x, output.Length);
         }
     }
 }
